acf8a842c61c3626b949aa6d883def533a7b36f8
Dawid <rogowiczdawid@gmail.com>
2017-06-01 22:57:53 +0200

    added files to dist folder next: babel for header script

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-import { navAttacher } from './scripts/nav.js';
+import { navAttacher } from './scripts/nav';
+import '../node_modules/p5/lib/p5.min';
+import './style.scss';
 
 $(function(){
-  
+
   var windowHeight = $(window).innerHeight();
   $('.parallax').css({'min-height': windowHeight/2+100});
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,56 @@
 var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin({
+  filename: 'style.css'
+});
 
 module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist'
-  }
-}
+    // publicPath: '/dist'
+  },
+  module: {
+      rules: [
+        {
+          test: /\.scss/,
+          use: extractPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+          })
+        },
+        {
+          test: /\.html/,
+          loader: 'html-loader'
+        },
+        {
+          test: /\.jpg$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'assets/',
+                publicPath: 'assets/'
+              }
+            }
+          ]
+        },
+        {
+          test: /\.css/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({}),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+      extractPlugin
+  ]
+};

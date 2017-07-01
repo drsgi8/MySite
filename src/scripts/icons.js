db6f00acccc699d11b2cc62ceba019b597a63a49
Dawid <rogowiczdawid@gmail.com>
2017-07-01 16:30:36 +0200

    mouse can move icons

--- src/scripts/icons.js
+++ src/scripts/icons.js
@@ -1,5 +1,7 @@
 $(function(){
   var width = window.innerWidth/3;
+  var offset = -2;
+  var mouseover = false;
   var icons = [];
   $.each($('.fa'), function(){
     icons.push($(this));
@@ -9,7 +11,16 @@ $(function(){
 
   window.onresize = resetIcons;
 
-  setInterval(moveLeft, 25);
+  setInterval(moveIcons, 25);
+
+  $('#icon-container').hover(function(){
+    mouseover = true;
+  }, function(){
+    mouseover = false;
+  }).mousemove(function(event){
+    console.log('asd');
+    offset = (event.pageX / window.innerWidth - 0.5) * 24;
+  });
 
   function resetIcons(){
     for(var i=0; i<icons.length; i++){
@@ -22,13 +33,20 @@ $(function(){
     }
   }
 
-  function moveLeft(){
-    for(var i=0; i<icons.length; i++){
-      var x = icons[i].offset().left - 2;
-      if(x < (-1) * width)
-        x = window.innerWidth;
-      icons[i].css({left: x});
+  function moveIcons(){
+    if(offset > 0){
+      for(var i=0; i<icons.length; i++){
+        var x = icons[i].offset().left + offset;
+        if(x < (-1) * width)
+          x = window.innerWidth;
+        else if(x > window.innerWidth)
+          x = (-1) * width;
+        icons[i].css({left: x});
+      }
+      if(!mouseover)
+        offset *= 0.98;
     }
   }
+
 
 });
